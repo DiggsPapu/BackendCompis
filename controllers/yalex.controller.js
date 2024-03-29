2e9f1d84805bc8c2700a130a96e9503a03aeb04d
DiggsPapu <diggs.papu@gmail.com>
2024-03-29 13:33:15 +0000

    update:Se realiza el handling para manejar el yalex analyzer asi mismo maneja el script que se envia y retorna un svg con la imagen del ast tree, a su vez tambien se realizo una get request handling del controller para obtener el DFA generado a partir de este analizador de yalex

--- controllers/yalex.controller.js
+++ controllers/yalex.controller.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 var YalexAnalyzer = require('../class/YalexAnalyzer.js');
 const { graphviz } = require('node-graphviz');
-const { drawTreeTokens } = require("./draw.functions.js");
+const { drawTreeTokens, drawGraphDFA } = require("./draw.functions.js");
 
 let yalex = null;
 function analyzeYalex(req, res){
     yalex = new YalexAnalyzer(req.body.content);
     graphviz.dot(drawTreeTokens(yalex.ast), 'svg').then((svg) => {fs.writeFileSync('./images/yalexAST.svg', svg);});
     // Send the tree as svg
-    fs.readFile('images/yalexAST.svg', 'utf8', (err, data) => {
+    fs.readFile('./images/yalexAST.svg', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             res.status(500).send('Internal Server Error');
@@ -22,6 +22,26 @@ function analyzeYalex(req, res){
         res.send(data);
     });
 };
+function getDFA(req, res){
+    if (yalex !== null){
+        graphviz.dot(drawGraphDFA(yalex.directDFA), 'svg').then((svg) => {fs.writeFileSync('./images/yalexDFA.svg', svg);});
+        // Send the dfa as svg
+        fs.readFile('images/yalexDFA.svg', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.status(500).send('Internal Server Error');
+                return;
+            }
+            
+            // Set the content type to SVG
+            res.set('Content-Type', 'image/svg+xml');
+            
+            // Send the SVG file in the response
+            res.send(data);
+        });
+    }
+}
 module.exports = {
-    analyzeYalex
+    analyzeYalex,
+    getDFA
 }
--- routes/yalex.route.js
+++ routes/yalex.route.js
@@ -2,4 +2,5 @@ var analyzeYalex = require('../controllers/yalex.controller.js');
 var express = require('express');
 var api = express.Router()
 api.post('/analyzeYalex/Ast', analyzeYalex.analyzeYalex);
+api.get('/analyzeYalex/DFA', analyzeYalex.getDFA);
 module.exports = api;
